3c3fc3da546ad3789960f2d9a3ee0b9e25b4d929
arthells <80214183+Asmin963@users.noreply.github.com>
2024-10-07 00:20:55 +0300

    Update linux_install.sh

--- linux_install.sh
+++ linux_install.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
-REPO_URL="https://github.com/Asmin963/PskovEduBot"
-REPO_NAME="PskovEduBot"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+MAGENTA='\033[0;35m'
+NC='\033[0m'
 
-echo "Клонирую репозиторий..."
-git clone "$REPO_URL"
+echo -e "${GREEN}Установка Python 3.12 и зависимостей...${NC}"
+sudo apt install -y python3.12 python3.12-dev python3.12-gdbm python3.12-venv
+wget https://bootstrap.pypa.io/get-pip.py -nc
+sudo python3.12 get-pip.py
 
-cd "$REPO_NAME" || exit
+rm -rf get-pip.py
 
-echo "Создаю виртуальное окружение и устанавливаю зависимости..."
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+echo -e "${GREEN}Установка git...${NC}"
+sudo apt install -y git
 
-echo "Установка pm2..."
+sudo rm -rf FunPayCardinal
+
+echo -e "${GREEN}Клонирование репозитория PskovEduBot...${NC}"
+git clone https://github.com/Asmin963/PskovEduBot
+
+echo -e "${GREEN}Переход в директорию проекта...${NC}"
+cd PskovEduBot  
+
+echo -e "${GREEN}Создаю виртуальное окружение и устанавливаю зависимости...${NC}"
+pip install -r reqierements.txt
+
+echo -e "${GREEN}Установка pm2...${NC}"
 npm install -g pm2
 
-echo "Запуск main.py через pm2..."
-pm2 start main.py --interpreter python3 --name your-app-name
+echo -e "${GREEN}Запуск main.py через pm2...${NC}"
+pm2 start main.py --interpreter python3.12 --name PskovEduBot
 
 pm2 save
 
 pm2 startup
 
-echo "Установка завершена!"
+echo -e "${GREEN}Установка завершена!${NC}"
